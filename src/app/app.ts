35089614c4e556cd9a39bfffb1b237486b7a186b
Paulo <adm.pauloricardo91@gmail.com>
2025-11-27 05:37:33 -0300

    criando-About-DionathanGomes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './pages/home/home';
 import { AboutAd } from "./pages/about-ad/about-ad";
+import { AboutDionathanGomes } from "./pages/about-dionathan-gomes/about-dionathan-gomes";
 
 @Component({
   selector: 'app-root',
-  imports: [Home, AboutAd],
+  imports: [Home, AboutAd, AboutDionathanGomes],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
